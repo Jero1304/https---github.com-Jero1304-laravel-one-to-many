1833cb531cdd2420a05417a79340e3bea61a7cc5
Jero1304 <calogeroleto13@gmail.com>
2023-04-26 15:40:02 +0200

    first commit

---
+++ laravel-one-to-many/resources/views/posts/index.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container py-5">
+        <div class="d-flex align-items-center">
+            <h1 class="me-auto py-3">Tutti i post</h1>
+
+
+            <div>
+                <a class="btn btn-primary" href="{{ route('posts.create') }}">Nuovo Post</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container py-5">
+        <table class="table table-striped table-inverse table-responsive">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Slug</th>
+                    <th>Data creazione</th>
+                    <th>Data Modifica</th>
+                    <th>Eliminato</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse($posts as $post)
+                    <tr>
+                        <td>{{ $post->id }}</td>
+                        <td>
+                            <a href="{{ route('posts.show', $post) }}">{{ $post->title }}</a>
+                        </td>
+                        <td>{{ $post->slug }}</td>
+                        <td>{{ $post->created_at }}</td>
+                        <td>{{ $post->updated_at }}</td>
+                        <td>{{ $post->trashed() ? 'Eliminato il: ' . $post->deleted_at : '' }}</td>
+                        <td>
+                            <div class="d-flex">
+                            
+                                <a class="btn btn-sm btn-secondary" href="{{ route('posts.edit', $post) }}">Edit</a>
+
+                                <form action="{{ route('posts.destroy', $post) }}" method="POST">
+                                    @csrf
+                                    @method('delete')
+                                    <input class="btn btn-sn btn-danger" type="submit" value="Delete">
+                                </form>
+
+                                @if ($post->trashed())
+                                    <form action="{{ route('posts.restore', $post) }}" method="POST">
+                                        @csrf
+                                        <input class="btn btn-sn btn-success" type="submit" value="Ripristina">
+                                    </form>
+                                @endif
+                            </div>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <th colspan="6">Nessun post trovato</th>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+@endsection
